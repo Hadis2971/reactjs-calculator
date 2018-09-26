3829a15733cfcee48d95337c486f83435502095b
Albert Hofmann <albhof@email.com>
2018-09-27 00:31:24 +0200

    restructur app display components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import Calculator from "./container/Calculator";
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        
+        <Calculator />
       </div>
     );
   }
--- src/container/Calculator.js
+++ src/container/Calculator.js
@@ -1,18 +1,63 @@
 import React, {Component} from "react";
 
+import Operand from "../components/Operand/operand";
+import Operator from "../components/Operator/operator";
+
 class Calculator extends Component {
     
     state = {
-        result = ""
+        result: ""
+    };
+
+    operandClickHandler = (value) => {
+        this.setState(prevState => ({
+            result: prevState.result + value
+        }));
+    };
+
+    operatorClickHandler = (value) => {
+
+        if(value === "."){
+            this.setState(prevState => ({
+                result: prevState.result + value
+            }));
+        }else{
+            this.setState({
+                result: value
+            });
+        }        
     };
 
     render(){
         return(
             <div>
+                <Operator value="CE"/>
+                <Operator value="C"/>
+                <Operator value="BK"/>
+                <Operator value="/"/>
 
+                <Operand  value="7"/>
+                <Operand  value="8"/>
+                <Operand  value="9"/>
+                <Operator value="*"/>
+
+                <Operand  value="4"/>
+                <Operand  value="5"/>
+                <Operand  value="6"/>
+                <Operator value="-"/>
+
+                <Operand  value="1"/>
+                <Operand  value="2"/>
+                <Operand  value="3"/>
+                <Operator value="+"/>
+
+                <Operator value="+-"/>
+                <Operand  value="0"/>
+                <Operator value="."/>
+                <Operator value="="/>
             </div>
         );
     }
 };
 
-export default calculator;
+export default Calculator;
