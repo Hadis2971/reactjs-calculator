b1c6eafdcc7137366ddde998b34304d079ee685a
Albert Hofmann <albhof@email.com>
2018-09-27 12:34:26 +0200

    remove assets folder use font-awesome instead

--- src/App.js
+++ src/App.js
@@ -7,6 +7,10 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+      <div id="hdr">
+      <i className="fas fa-calculator" style={{fontSize:"3em", marginRight:"0.19em"}}></i>
+        <h1 style={{display:"inline", verticalAlign:"super"}}>ReactJS Calculator</h1>
+      </div>
         <Calculator />
       </div>
     );
